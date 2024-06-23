2634e17c7b4940d3f93e7d350e20b317c1357529
azizaturrohma <azizaturrohmablt234@gmail.com>
2024-06-23 23:14:21 +0700

    delete FK've been pivot tables

--- database/migrations/2024_06_23_091757_create_reportings_table.php
+++ database/migrations/2024_06_23_091757_create_reportings_table.php
@@ -22,16 +22,10 @@ return new class extends Migration
             $table->text('chronology');
             $table->unsignedBigInteger('reported_status_id');
             $table->foreign('reported_status_id')->references('id')->on('reported_statuses')->onUpdate('cascade')->onDelete('restrict');
-            $table->unsignedBigInteger('disability_type_id');
-            $table->foreign('disability_type_id')->references('id')->on('disability_types')->onUpdate('cascade')->onDelete('restrict');
             $table->string('optional_disability_type')->nullable();
-            $table->unsignedBigInteger('reporting_reason_id');
-            $table->foreign('reporting_reason_id')->references('id')->on('reporting_reasons')->onUpdate('cascade')->onDelete('restrict');
             $table->text('optional_reporting_reason')->nullable();
             $table->string('optional_phone_number')->nullable();
             $table->string('optional_email')->nullable();
-            $table->unsignedBigInteger('victim_requirement_id');
-            $table->foreign('victim_requirement_id')->references('id')->on('victim_requirements')->onUpdate('cascade')->onDelete('restrict');
             $table->string('optional_victim_requirement')->nullable();
             $table->string('reporter_signature')->nullable();
             $table->timestamps();
